3d03f5e46adee18af2c4a7b974881d779bded2a1
boniface <boniface@dev>
2013-08-16 05:57:48 +0200

    added Tabs for the People Meta Data

--- src/main/java/zm/hashcode/tics/client/web/content/system/peoplemetadata/PeopleMetaDataMenu.java
+++ src/main/java/zm/hashcode/tics/client/web/content/system/peoplemetadata/PeopleMetaDataMenu.java
@@ -4,13 +4,16 @@
  */
 package zm.hashcode.tics.client.web.content.system.peoplemetadata;
 
-import zm.hashcode.tics.client.web.content.users.*;
 import zm.hashcode.tics.client.web.Menu;
 import com.vaadin.ui.VerticalLayout;
 import zm.hashcode.tics.client.web.TicsMain;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.GenderTab;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.IdentificationTypeTabTab;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.LanguageProficiencyTab;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.LanguageTab;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.MaritalStatusTab;
+import zm.hashcode.tics.client.web.content.system.peoplemetadata.tabs.RaceTab;
 import zm.hashcode.tics.client.web.content.users.tabs.ResetTab;
-import zm.hashcode.tics.client.web.content.users.tabs.RoleTab;
-import zm.hashcode.tics.client.web.content.users.tabs.UserTab;
 
 /**
  *
@@ -21,32 +24,70 @@ public class PeopleMetaDataMenu extends Menu {
     public PeopleMetaDataMenu(TicsMain app, String selectedTab) {
         super(app, selectedTab);
 
-        final VerticalLayout userTab = new VerticalLayout();
-        userTab.setMargin(true);
-        userTab.addComponent(new UserTab(getMain()));
+        final VerticalLayout genderTab = new VerticalLayout();
+        genderTab.setMargin(true);
+        genderTab.addComponent(new GenderTab(getMain()));
 
 
-        final VerticalLayout roleTab = new VerticalLayout();
-        roleTab.setMargin(true);
-        roleTab.addComponent(new RoleTab(getMain()));
+        final VerticalLayout identificationTypeTab = new VerticalLayout();
+        identificationTypeTab.setMargin(true);
+        identificationTypeTab.addComponent(new IdentificationTypeTabTab(getMain()));
 
-        final VerticalLayout resetTab = new VerticalLayout();
-        resetTab.setMargin(true);
-        resetTab.addComponent(new ResetTab(getMain()));
+        final VerticalLayout languageProciencyTab = new VerticalLayout();
+        languageProciencyTab.setMargin(true);
+        languageProciencyTab.addComponent(new LanguageProficiencyTab(getMain()));
 
-        getTab().addTab(userTab, "Add System USERS", null);
-        getTab().addTab(roleTab, "Add System ROLES", null);
-        getTab().addTab(resetTab, "Reset CREDENTIALS", null);
+        final VerticalLayout languageTab = new VerticalLayout();
+        languageTab.setMargin(true);
+        languageTab.addComponent(new LanguageTab(getMain()));
+
+
+        final VerticalLayout maritalStatusTab = new VerticalLayout();
+        maritalStatusTab.setMargin(true);
+        maritalStatusTab.addComponent(new MaritalStatusTab(getMain()));
+
+        final VerticalLayout raceTab = new VerticalLayout();
+        raceTab.setMargin(true);
+        raceTab.addComponent(new RaceTab(getMain()));
+
+        final VerticalLayout titleTab = new VerticalLayout();
+        titleTab.setMargin(true);
+        titleTab.addComponent(new ResetTab(getMain()));
+
+        getTab().addTab(genderTab, "GENDER", null);
+        getTab().addTab(identificationTypeTab, "Identification TYPE", null);
+        getTab().addTab(languageProciencyTab, "Language  PROFICIENCY", null);
+
+        getTab().addTab(languageTab, "LANGUAGE", null);
+        getTab().addTab(maritalStatusTab, "Marital STATUS", null);
+        getTab().addTab(raceTab, "RACE", null);
+
+        getTab().addTab(titleTab, "TITLE", null);
+
+
 
         switch (selectedTab) {
             case "LANDING":
-                getTab().setSelectedTab(userTab);
+                getTab().setSelectedTab(genderTab);
                 break;
-            case "ROLES":
-                getTab().setSelectedTab(roleTab);
+            case "IDENTITY":
+                getTab().setSelectedTab(identificationTypeTab);
                 break;
-            case "RESETS":
-                getTab().setSelectedTab(roleTab);
+            case "PROFICIENCY":
+                getTab().setSelectedTab(languageProciencyTab);
+                break;
+
+            case "LANGUAGE":
+                getTab().setSelectedTab(languageTab);
+                break;
+            case "MARITAL":
+                getTab().setSelectedTab(maritalStatusTab);
+                break;
+            case "RACE":
+                getTab().setSelectedTab(raceTab);
+                break;
+            case "TITLE":
+                getTab().setSelectedTab(titleTab);
                 break;
         }
         addComponent(getTab());

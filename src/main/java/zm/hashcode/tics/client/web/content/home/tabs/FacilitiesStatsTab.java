902e1194b05473c91c35a7c3a468bc0c6d323eb3
boniface <boniface@dev>
2013-09-13 10:35:00 +0200

    Refactored QR Codes file

--- src/main/java/zm/hashcode/tics/client/web/content/home/tabs/FacilitiesStatsTab.java
+++ src/main/java/zm/hashcode/tics/client/web/content/home/tabs/FacilitiesStatsTab.java
@@ -4,6 +4,10 @@
  */
 package zm.hashcode.tics.client.web.content.home.tabs;
 
+import com.vaadin.tapio.googlemaps.GoogleMap;
+import com.vaadin.tapio.googlemaps.client.GoogleMapMarker;
+import com.vaadin.tapio.googlemaps.client.LatLon;
+import com.vaadin.ui.Button;
 import com.vaadin.ui.VerticalLayout;
 import zm.hashcode.tics.client.web.TicsMain;
 
@@ -14,8 +18,26 @@ import zm.hashcode.tics.client.web.TicsMain;
 public class FacilitiesStatsTab extends VerticalLayout {
 
     private final TicsMain main;
+    private GoogleMap googleMap;
+    private GoogleMapMarker kakolaMarker;
+    private final String apiKey = "AIzaSyAmEPi9fulTtG2wTggSn9b-dbCfhtnJaaM";
 
     public FacilitiesStatsTab(TicsMain main) {
+        setSizeFull();
+        kakolaMarker = new GoogleMapMarker("DRAGGABLE: Kakolan vankila", new LatLon(60.44291, 22.242415), true);
+        googleMap = new GoogleMap(new LatLon(60.440963, 22.25122), 10.0, apiKey);
+
         this.main = main;
+
+        googleMap.setSizeFull();
+
+        googleMap.addMarker(kakolaMarker);
+
+        googleMap.setMinZoom(4.0);
+        googleMap.setMaxZoom(16.0);
+
+        addComponent(googleMap);
+
+
     }
 }
